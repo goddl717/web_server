9142df4fdcf5c5e01c3d018b2a435fb83cc59a01
goddl717 <goddl717@naver.com>
2019-11-26 13:07:45 +0900

    css 수정
    css 수정

--- screen.js
+++ screen.js
@@ -2,17 +2,24 @@ var express = require('express')
 var cors = require('cors')
 var app = express()
 var path = require('path');
+var passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 
+app.use(session({ secret: 'SECRET_CODE', resave: true, saveUninitialized: false }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(cors())
 app.use('/assets',express.static('assets'))
 
-app.get('/', function (req, res, next) {
+
+
+app.get('/login', function (req, res, next) {
   res.sendFile(path.join(__dirname ,'/index.html'));
 })
 
 app.get('/regist', function (req, res, next) {
     console.log("regist!");
-    
     res.sendFile(path.join(__dirname ,'/regist.html'));
 })
 
