b74a93e22b9030cac52cc9ed0c99673bdac106cb
goddl717 <goddl717@naver.com>
2019-11-29 02:37:46 +0900

    asd
    asd

--- screen.js
+++ screen.js
@@ -5,6 +5,46 @@ var path = require('path');
 var passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+var express = require('express');
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var {PythonShell} = require('python-shell');
+var http = require('http'); 
+var fs = require('fs'); // 파일 읽기, 쓰기 등 을 할 수 있는 모듈
+var app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+var addUserRouter = require('./user/addUser');
+var loginUserRouter = require('./user/checkId');
+var initDiaRouter = require('./dia/initDia');
+var updatePassRouter = require('./user/updatePass');
+var pythonRouter = require('./pyth/ImageToText')
+var storeRouter = require('./dia/storeData');
+var storeNegativeRouter = require('./dia/storeDataNegative');
+//var screenRouter = require('./screen/screen');
+
+//var storeTextRouter = require('./pyth/storeText');
+
+
+var pool = mysql.createPool({
+   connectionLimit :10,
+    host : 'localhost',
+    user : 'root',
+    password : 'eorn',
+    port : 3306,
+    database : 'hang'
+})
+
+//파일 분리.
+app.use('/user/addUser',addUserRouter);
+app.use('/user/checkId',loginUserRouter);
+app.use('/dia/initDia',initDiaRouter);
+app.use('/user/updatePass',updatePassRouter);
+app.use('/pyth/ImageToText',pythonRouter);
+app.use('/dia/storeData',storeRouter);
+app.use('/dia/storeDataNegative',storeNegativeRouter);
+//app.use('/screen/screen',screenRouter);
 
 app.use(session({ secret: 'SECRET_CODE', resave: true, saveUninitialized: false }));
 app.use(passport.initialize());
